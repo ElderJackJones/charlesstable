38f1e6a0935ec7cac41b972b37be313973010015
Jack <jpjones@missionary.org>
2025-09-06 21:06:53 -0400

    Tauri working

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
-	optimizeDeps: { include: ['@skeletonlabs/skeleton-svelte', '@skeletonlabs/skeleton', 'lucide-svelte'] }
+	optimizeDeps: { include: ['@skeletonlabs/skeleton-svelte', '@skeletonlabs/skeleton', 'lucide-svelte'] },
+	// Tauri expects a fixed port, fail if that port is not available
+	server: {
+		port: 5173,
+		strictPort: true,
+		watch: {
+			// 3. tell vite to ignore watching `src-tauri`
+			ignored: ['**/src-tauri/**']
+		}
+	}
 });
